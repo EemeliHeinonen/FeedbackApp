2f37a4f7d1c9ca344e18e27bdcf6818387b61efb
Eemeli Heinonen <eemeli.heinonen@metropolia.fi>
2016-04-14 16:11:21 +0300

    Made more views & classes

---
+++ mockup2/StudentFeedbackViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mockup2
+//
+//  Created by Mortti Aittokoski on 21.3.2016.
+//  Copyright © 2016 Mortti Aittokoski. All rights reserved.
+//
+
+import UIKit
+
+
+class StudentFeedbackViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UINavigationControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
