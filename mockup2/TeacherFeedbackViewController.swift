09e1e969b8241522196a45676a3a1ebb9c964f9a
Mortti Aittokoski <morttiaittokoski@mortti-macbook.local>
2016-04-15 10:11:54 +0300

    pe aamun pikkumuutoksia

---
+++ mockup2/TeacherFeedbackViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TeacherFeedbackViewController.swift
+//  mockup2
+//
+//  Created by Mortti Aittokoski on 15.4.2016.
+//  Copyright © 2016 Mortti Aittokoski. All rights reserved.
+//
+
+import UIKit
+
+
+class TeacherFeedbackViewController: UIViewController, UINavigationControllerDelegate, UITableViewDelegate {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+}
