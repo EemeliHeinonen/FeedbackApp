fb192063809817d67793727f7c0162fdef7ce956
Eemeli Heinonen <eemeli.heinonen@metropolia.fi>
2016-04-25 13:24:30 +0300

    Student signup CoreData business done + CoreData classes made.

---
+++ mockup2/CoreDataFolder/Lesson+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  Lesson+CoreDataProperties.swift
+//  mockup2
+//
+//  Created by Eemeli Heinonen on 25/04/16.
+//  Copyright © 2016 Mortti Aittokoski. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Lesson {
+
+    @NSManaged var lessonFeedback: String?
+    @NSManaged var lessonRating: NSNumber?
+    @NSManaged var subject: String?
+    @NSManaged var timeAndDate: NSDate?
+    @NSManaged var relationship: Classroom?
+    @NSManaged var relationship1: NSManagedObject?
+    @NSManaged var relationship2: NSSet?
+    @NSManaged var relationship3: NSSet?
+
+}
---
+++ mockup2/CoreDataFolder/Topic.swift
@@ -0,0 +1,17 @@
+//
+//  Topic.swift
+//  mockup2
+//
+//  Created by Eemeli Heinonen on 25/04/16.
+//  Copyright © 2016 Mortti Aittokoski. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Topic: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
